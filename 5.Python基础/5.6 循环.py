f8ced3c244d5ee2e0e74237e9f144b8d442b4992
kivet <kivet.whx@petalmail.com>
2025-10-30 16:30:32 +0800

    fix: 5.6 循环

--- 5.Python基础/5.6 循环.py
+++ 5.Python基础/5.6 循环.py
@@ -5,11 +5,15 @@
 # python中的循环有两种
 
 # 1. for...in循环
-# 可以依次把list或tuple中的每一个元素迭代出来
+# 常见的 for...in 循环可遍历的数据类型有：列表（list）、元祖（tuple）、字符串（str）、字典（dict）等等（不止这些）。
 
 # names = ['A', 'B', 'C']
 # for name in names:
 #     print(name)
+
+# obj = { 'a': 1, 'b': 2, 'c': 3 }
+# for item in obj:
+#     print(item)
 
 
 # 求和
@@ -18,18 +22,29 @@
 #     sum1 = sum1 + n
 # print(sum1) # ==> 55
 
+
 # 拓展：Python中，快速生成[0, ..., n]
 # print(list(range(11))) # ==> [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 # range()：可以生成一个整数序列
-    # print(range(5)) # ==> range(0, 5)
+# print(range(5)) # ==> range(0, 5)
 # list()：可以将整数序列转换成list
+# print(list(range(5))) # ==> [0, 1, 2, 3, 4]
+# print('list(range(5))，相当于：list(range(0， 5))，执行结果为：',list(range(0, 5))) # ==> list(range(5))，相当于：list(range(0， 5))，执行结果为： [0, 1, 2, 3, 4]
 
 # for...in 也可以直接循环range()
 # sum1 = 0
 # for n in range(11):
 #     sum1 = sum1 + n
-# print(sum1) # ==> 55
+# print('sum1:', sum1) # ==> sum1: 55
+
+# sum2 = 0
+# for n in list(range(11)):
+#     sum2 = sum2 + n
+# print('sum2:', sum2) # ==> sum2: 55
+
+
+
 
 # 2. while 循环
 # sum2 = 0
@@ -38,6 +53,8 @@
 #     sum2 = sum2 + n
 #     n = n + 1
 # print(sum2) # ==> 55
+
+
 
 # 3. break 提前结束循环
 # n = 1
@@ -48,6 +65,8 @@
 #     n = n + 1
 # # 无if判断：依次打印1，2，...，9
 # # 有if判断：依次打印1,2,3,4，结束循环。
+
+
 
 # 4. continue 跳过当前循环，开始下一次循环
 # n = 0
