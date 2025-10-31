494b1b9b9759daee26e45d747312c6928e7621ac
kivet <kivet.whx@petalmail.com>
2025-10-31 14:40:16 +0800

    fix: 7.2 迭代

--- 7.高级特性/7.2 迭代.py
+++ 7.高级特性/7.2 迭代.py
@@ -4,16 +4,17 @@
 
 # 如果给定一个list或tuple，我们可以通过for循环来遍历这个list或tuple，这种遍历我们称为迭代（Iteration）。
 
-# Python代码
+# 在Python中，迭代是通过for ... in来完成的
 # L = ['a', 'b', 'c', 'd']
 # for item in L:
 #     print(item)
 
-# # JS 代码
+# 而很多语言比如C语言，迭代list是通过下标完成的，比如C代码：
 # L = ['a', 'b', 'c', 'd']
 # for (i = 0; i < L.length; i++) {
-#     console.log(list[i])
+#     n = list[i];
 # }
+
 
 # 可以看出，Python的for循环抽象程度要高于C的for循环，
 # 因为Python的for循环不仅可以用在list或tuple上，还可以作用在其他可迭代对象上。
@@ -22,22 +23,43 @@
 # d = {'a': 1, 'b': 2, 'c': 3}
 # for key in d:
 #     print(key, d[key])
-# # ===>
-# # a 1
-# # b 2
-# # c 3
+# ===>
+# a 1
+# b 2
+# c 3
 
 # 有时候，例如上面dict，打印出来的顺序有可能不是a,b,c这种依次顺序，可能是a,c,b，或者其它顺序。
-# 这是隐藏dict的存储不是安装list的方式顺序排序的，所以迭代出来的结果顺序可能不一样。
+# 这是因为dict的存储不是按照list的方式顺序排序的，所以迭代出来的结果顺序可能不一样。
 
 # 默认情况下，dict迭代的是key。
 # 如果要迭代value，可以用for value in d.values()，
+# d = {'a': 1, 'b': 2, 'c': 3}
+# for value in d.values():
+#     print(value)
+# ===>
+# 1
+# 2
+# 3
+
 # 如果要同时迭代key和value，可以用for k, v in d.items()。
+# d = {'a': 1, 'b': 2, 'c': 3}
+# for k, v in d.items():
+#     print(k,v)
+# ===>
+# a 1
+# b 2
+# c 3
+
 
 # 字符串也是可迭代对象，因此，也可以用for循环进行迭代
 # s = 'ABCD'
 # for item in s:
 #     print(item)
+# ==>
+# A
+# B
+# C
+# D
 
 # 总结：在Python中，只要是可迭代对象，都可以使用for循环进行迭代。
 # 常见可迭代对象有：list、tuple、dict、字符串
@@ -52,6 +74,7 @@
 # print(isinstance('abc', Iterable)) # ==> True
 # print(isinstance(123, Iterable)) # ==> False
 
+
 # 如何对list实现类似Java那种下标循环？
 # 答：Python内置的enumerate函数可以把一个list变成索引-元素对，这样就可以在for循环中同时迭代索引和元素本身：
 # L = ['A', 'B', 'C']
@@ -62,6 +85,7 @@
 # # 1 B
 # # 2 C
 
+
 # for循环里，同时引用了两个变量，在Python里是很常见的，比如下面的代码：
 # for x, y in [(1, 1), (2, 4), (3, 9)]:
 #     print(x, y)
@@ -69,3 +93,21 @@
 # # 1 1
 # # 2 4
 # # 3 9
+
+# for x, y, z in [(1, 5, 15), (2, 6, 26), (3, 7, 37)]:
+#     print(x, y, z)
+# # ==>
+# # 1 5 15
+# # 2 6 26
+# # 3 7 37
+
+# for x, y, z in [(1, 5, 15), (2, 6), (3, 7)]:
+#     print(x, y, z)
+# ==>
+# 能打印出1 5 15
+# 剩下的报错
+
+# for x, y in [(1, 5, 15), (2, 6), (3, 7)]:
+#     print(x, y)
+# ==>
+# 直接报错
